b56d66a991a12fea5689d5804d55b733e0cbdf3d
ozen-hub <info.ozenhub@gmail.com>
2024-04-27 22:03:27 +0530

    4

--- widgets/ProgramWidget.tsx
+++ widgets/ProgramWidget.tsx
@@ -1,53 +1,56 @@
-import {ScrollView, StyleSheet, Text, View} from "react-native";
-import SubjectWidget from "./SubjectWidget.tsx";
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import SubjectWidget from './SubjectWidget.tsx';
 import moment from 'moment';
-const ProgramWidget = ({navigation,data}) => {
-    return (
-        <View style={styles.view}>
-            <Text style={styles.name}>{data.name}</Text>
-            <Text style={styles.amount}>{data.price}</Text>
+const ProgramWidget = ({navigation, data}) => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.name}>{data.name}</Text>
+      <Text style={styles.amount}>{data.price}</Text>
 
-            <View style={styles.date}>
-                <Text style={{color: 'white'}}>{moment(data.startDate).format('YYYY-MM-DD')}</Text>
-            </View>
+      <View style={styles.date}>
+        <Text style={{color: 'white'}}>
+          {moment(data.startDate).format('YYYY-MM-DD')}
+        </Text>
+      </View>
 
-            <ScrollView style={styles.scroll} horizontal={true}>
-                {
-                    data.subjects?.map((subject,index)=>(
-                        <SubjectWidget key={index} navigation={navigation} subject={subject.name}/>
-                    ))
-                }
-            </ScrollView>
-
-        </View>
-    )
-}
+      <ScrollView style={styles.scroll} horizontal={true}>
+        {data.subjects?.map((subject, index) => (
+          <SubjectWidget
+            key={index}
+            navigation={navigation}
+            subject={subject.name}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    scroll: {
-        marginTop: 20,
-    },
-    date: {
-        width: 120,
-        height: 35,
-        justifyContent: "center",
-        alignItems: 'center',
-        backgroundColor: '#d35400',
-        borderRadius: 50,
-        marginTop: 15,
-    },
-    amount: {
-        color: '#c0392b'
-    },
-    name: {
-        fontSize: 20,
-        color: '#000'
-    },
-    view: {
-        width: '100%',
-        padding: 8,
-        backgroundColor: '#bdc3c7',
-        borderRadius: 3,
-        marginBottom:10
-    }
+  scroll: {
+    marginTop: 20,
+  },
+  date: {
+    width: 120,
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d35400',
+    borderRadius: 50,
+    marginTop: 15,
+  },
+  amount: {
+    color: '#c0392b',
+  },
+  name: {
+    fontSize: 20,
+    color: '#000',
+  },
+  view: {
+    width: '100%',
+    padding: 8,
+    backgroundColor: '#bdc3c7',
+    borderRadius: 3,
+    marginBottom: 10,
+  },
 });
 export default ProgramWidget;
